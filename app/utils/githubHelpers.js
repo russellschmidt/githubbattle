8f7b72bc06b07a35a8422890e5022a36df2321dc
Russell Schmidt <russell.schmidt@me.com>
2017-03-13 12:44:44 -0700

    implement axios

---
+++ app/utils/githubHelpers.js
@@ -0,0 +1,27 @@
+var axios = require('axios')
+// in case you get rate limited
+var id = "YOUR_CLIENT_ID"
+var sec = "YOUR_SECRET_ID"
+var param = "?client_id=" + id + "&client_secret=" + sec;
+
+function getUserInfo(username) {
+  return axios.get('https://api.github.com/users/' + username + param)
+}
+
+var helpers = {
+  getPlayersInfo: function(players) {
+    // fetch data from github via Axios by looping over the players array w/ dot map
+    // calling getUserInfo and returning the data
+    return axios.all(players.map(function(username){
+      return getUserInfo(username)
+    })).then(function(info){
+      return info.map(function(user){
+        return user.data
+      }).catch(function(err){
+        console.log('error in getPlayersInfo', err)
+      })
+    })
+  }
+}
+
+module.exports = helpers;
