72c01c4711560a70728201bb0ceffaa929ef5b82
Russell Schmidt <russell.schmidt@me.com>
2017-03-15 12:17:43 -0700

    got battle layout UI done

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,6 +1,9 @@
 var React = require('react')
 var PropTypes = React.PropTypes
-var transparentBg = require('../styles').transparentBg
+var styles = require('../styles')
+var Link = require('react-router').Link
+var UserDetails = require('./UserDetails')
+var UserDetailsWrapper = require('./UserDetailsWrapper')
 
 function puke(object) {
   return <pre>{JSON.stringify(object, null, ' ')}</pre>
@@ -12,22 +15,26 @@ function ConfirmBattle (props) {
     : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
         <h1>Confirm Players</h1>
         <div className="col-sm-8 col-sm-offset-2">
-          <div className="col-sm-6">
-            <p className="lead">Player One</p>
-            PLAYER ONE INFO
-          </div>
-          <div className="col-sm-6">
-            <p className="lead">Player Two</p>
-            PLAYER TWO INFO
-          </div>
+          <UserDetailsWrapper header={'Player One'}>
+            <UserDetails info={props.playersInfo[0]} />
+          </UserDetailsWrapper>
+          <UserDetailsWrapper header={'Player Two'}>
+            <UserDetails info={props.playersInfo[1]} />
+          </UserDetailsWrapper>
         </div>
 
         <div className="col-sm-8 col-sm-offset-2">
           <div className="col-sm-12" style={styles.space}>
-            INITIATE BATTLE BUTTON
+            <button type="button" className="btn btn-lg btn-success" onClick={props.onInitiateBattle}>
+              Initiate Battle
+            </button>
           </div>
           <div className="col-sm-12" style={styles.space}>
-            LINK TO /PLAYERONE
+            <Link to='/playerOne'>
+              <button type='button' className='btn btn-lg btn-danger'>
+                Reselect Players
+              </button>
+            </Link>
           </div>
         </div>
       </div>
