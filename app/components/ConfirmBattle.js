c383212c37e893a3f97e0d0da1fc744087f9403f
Russell Schmidt <russell.schmidt@me.com>
2017-03-15 10:59:35 -0700

    flesh  out battle element jsx

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,4 +1,6 @@
 var React = require('react')
+var PropTypes = React.PropTypes
+var transparentBg = require('../styles').transparentBg
 
 function puke(object) {
   return <pre>{JSON.stringify(object, null, ' ')}</pre>
@@ -7,7 +9,34 @@ function puke(object) {
 function ConfirmBattle (props) {
   return props.isLoading === true
     ? <p> LOADING... </p>
-    : <div> CONFIRM BATTLE!: {puke(props)} </div>
+    : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+        <h1>Confirm Players</h1>
+        <div className="col-sm-8 col-sm-offset-2">
+          <div className="col-sm-6">
+            <p className="lead">Player One</p>
+            PLAYER ONE INFO
+          </div>
+          <div className="col-sm-6">
+            <p className="lead">Player Two</p>
+            PLAYER TWO INFO
+          </div>
+        </div>
+
+        <div className="col-sm-8 col-sm-offset-2">
+          <div className="col-sm-12" style={styles.space}>
+            INITIATE BATTLE BUTTON
+          </div>
+          <div className="col-sm-12" style={styles.space}>
+            LINK TO /PLAYERONE
+          </div>
+        </div>
+      </div>
+}
+
+ConfirmBattle.PropTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  onInitiateBattle: PropTypes.func.isRequired,
+  playersInfo: PropTypes.array.isRequired
 }
 
 module.exports = ConfirmBattle
